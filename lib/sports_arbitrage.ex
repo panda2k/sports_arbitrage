6bc4eb8721eaa5d5988d8982fe3872a700c30ded
panda2k <thelowkeypanda@gmail.com>
2024-04-25 23:11:55 -0700

    initial commit

---
+++ lib/sports_arbitrage.ex
@@ -0,0 +1,18 @@
+defmodule SportsArbitrage do
+  @moduledoc """
+  Documentation for `SportsArbitrage`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SportsArbitrage.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sports_arbitrage_test.exs
@@ -0,0 +1,8 @@
+defmodule SportsArbitrageTest do
+  use ExUnit.Case
+  doctest SportsArbitrage
+
+  test "greets the world" do
+    assert SportsArbitrage.hello() == :world
+  end
+end
